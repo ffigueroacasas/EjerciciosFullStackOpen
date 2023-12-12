20b716f75ffbfcbd0a430024eda51d30c7434e24
Francisco Figueroa Casas <franciscofigueroacasas@gmail.com>
2023-12-12 14:28:32 -0300

    exercise 1.8 and 1.9 completed

--- Part 1/unicafe/src/App.jsx
+++ Part 1/unicafe/src/App.jsx
@@ -12,24 +12,34 @@ const Display = ({text, number}) => {
   )
 }
 
+const Stats = ({good, bad, neutral}) => {
+  const loadAverage = () => (good - bad) / (good + bad + neutral)
+  
+
+  const loadProportion = () => (good / (good + bad + neutral)) * 100
+
+  if (good != 0 || bad != 0 || neutral != 0){
+    return (
+      <div>
+        <h2>Stats: </h2>
+        <Display text="Good Reviews" number={good} />
+        <Display text="Bad Reviews" number={bad} />
+        <Display text="Neutral Reviews" number={neutral} />
+        <Display text="Total" number={good + bad + neutral}/>
+        <Display text="Average" number={loadAverage()}/>
+        <Display text="Positive" number={loadProportion() + '%'} />
+      </div>
+    )
+  }
+  else return (
+    <h2>No feedback given</h2>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  const loadAverage = () => {
-    if (good + bad + neutral != 0){
-      return  (good - bad) / (good + bad + neutral)
-    }
-    return 0
-  }
-
-  const loadProportion = () => {
-    if (good + bad + neutral != 0){
-      return  good / (good + bad + neutral) * 100
-    }
-    return 0
-  }
 
   return (
     <div>
@@ -37,13 +47,7 @@ const App = () => {
       <Button handleClick={() => setGood(good + 1)} text="Good" />
       <Button handleClick={() => setBad(bad + 1)} text="Bad" />
       <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral" />
-      <h2>Stats: </h2>
-      <Display text="Good Reviews" number={good} />
-      <Display text="Bad Reviews" number={bad} />
-      <Display text="Neutral Reviews" number={neutral} />
-      <Display text="Total" number={good + bad + neutral}/>
-      <Display text="Average" number={loadAverage()}/>
-      <Display text="Positive" number={loadProportion() + '%'} />
+      <Stats good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
