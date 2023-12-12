4e3149dcaa9d93e5005add8ca55851cfcf2f33df
Francisco Figueroa Casas <franciscofigueroacasas@gmail.com>
2023-12-12 14:11:34 -0300

    exercise 1.7 completed

--- Part 1/unicafe/src/App.jsx
+++ Part 1/unicafe/src/App.jsx
@@ -13,10 +13,23 @@ const Display = ({text, number}) => {
 }
 
 const App = () => {
-  // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
+  const loadAverage = () => {
+    if (good + bad + neutral != 0){
+      return  (good - bad) / (good + bad + neutral)
+    }
+    return 0
+  }
+
+  const loadProportion = () => {
+    if (good + bad + neutral != 0){
+      return  good / (good + bad + neutral) * 100
+    }
+    return 0
+  }
 
   return (
     <div>
@@ -28,6 +41,9 @@ const App = () => {
       <Display text="Good Reviews" number={good} />
       <Display text="Bad Reviews" number={bad} />
       <Display text="Neutral Reviews" number={neutral} />
+      <Display text="Total" number={good + bad + neutral}/>
+      <Display text="Average" number={loadAverage()}/>
+      <Display text="Positive" number={loadProportion() + '%'} />
     </div>
   )
 }
