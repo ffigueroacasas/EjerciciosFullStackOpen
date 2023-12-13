47da0d64242a68439d09261e5bf914951dd35480
Francisco Figueroa Casas <franciscofigueroacasas@gmail.com>
2023-12-13 19:51:56 -0300

    exercise 2.8 completed

--- Part 2/phoneGuide/src/App.jsx
+++ Part 2/phoneGuide/src/App.jsx
@@ -2,19 +2,21 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '012-3456789' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
     if (persons.findIndex((person) =>person.name === newName.trim()) === -1){
-      setPersons(persons.concat({name: newName}))
+      setPersons(persons.concat({name: newName, number: newNumber}))
     }
     else{
       window.alert(`${newName.trim()} is already on the phone book`)
     }
     setNewName('')
+    setNewNumber('')
   }
 
   return (
@@ -23,6 +25,7 @@ const App = () => {
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={(event) => setNewName(event.target.value)}/>
+          number: <input type="number" value={newNumber} onChange={(event) => setNewNumber(event.target.value)}/>
         </div>
         <div>
           <button type="submit">add</button>
@@ -30,7 +33,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map((person) =><p key={person.name}>{person.name}</p>)}
+        {persons.map((person) =><p key={person.name}>{person.name} {person.number}</p>)}
       </div>
     </div>
   )
