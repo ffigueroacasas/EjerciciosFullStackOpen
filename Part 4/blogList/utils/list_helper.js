dd0b1716dd6643c935a1ab1d690b0d1338ef54cc
Francisco Figueroa Casas <franciscofigueroacasas@gmail.com>
2023-12-31 10:42:05 -0300

    exercise 4.5 completed

--- Part 4/blogList/utils/list_helper.js
+++ Part 4/blogList/utils/list_helper.js
@@ -6,7 +6,31 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) =>  sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  let favorite = null
+  blogs.forEach(blog => {
+    if (favorite){
+      if (blog.likes > favorite.likes){
+        favorite = {
+        title: blog.title, 
+        author: blog.author,
+        likes: blog.likes
+      }
+      }
+    }
+    else{
+      favorite = {
+        title: blog.title, 
+        author: blog.author,
+        likes: blog.likes
+      }
+    }
+  })
+  return favorite
+}
+
 module.exports = {
   dummy, 
-  totalLikes
+  totalLikes, 
+  favoriteBlog
 }
