b3933e738f7778485230e48564529d8f9bd26562
Francisco Figueroa Casas <franciscofigueroacasas@gmail.com>
2024-01-04 15:54:39 -0300

    exercise 4.18 completed

--- Part 4/blogList/app.js
+++ Part 4/blogList/app.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const errorHandler = require('./utils/middlewares')
 
 const mongoUrl = `mongodb+srv://end-user:Figueroa1907@firstcluster.6wq2ngu.mongodb.net/`
@@ -14,6 +15,7 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 app.use(errorHandler)
 
 module.exports = app
