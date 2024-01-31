9b0df0ce7aa3681cc2bcdd5af854474c878a28b6
Francisco Figueroa Casas <franciscofigueroacasas@gmail.com>
2024-01-31 12:01:30 -0300

    exercise 5.16 completed

---
+++ Part 5/bloglist/src/components/CreateBlogForm.test.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import CreateBlogForm from './CreateBlogForm';
+import '@testing-library/jest-dom/extend-expect'
+import { fireEvent, render } from '@testing-library/react'
+
+describe('CreateBlogForm component', () => {
+  let component
+  test('when the form is submitted, the handler function is called', () => {
+   const addBlogMock = jest.fn()
+    component = render(
+    <CreateBlogForm addBlog={addBlogMock} />
+   )
+
+  const input = component.container.querySelector('buton')
+  const form = component.container.querySelector('form')
+
+  fireEvent.change(input, { 
+    target: { value: 'test blog entry' } 
+  })
+  fireEvent.submit(form)
+
+  expect(addBlogMock.mock.calls).toHaveLength(1)
+  expect(addBlogMock.mock.calls[0][0].content).toBe('test blog entry' )
+  });
+});
