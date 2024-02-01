2b38c2b97306182d617e9614f78ef8a6a02db9eb
Francisco Figueroa Casas <franciscofigueroacasas@gmail.com>
2024-02-01 16:41:59 -0300

    exercise 5.17 completed

--- Part 4/blogList/app.js
+++ Part 4/blogList/app.js
@@ -6,6 +6,7 @@ const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
 const middlewares = require('./utils/middlewares')
+const testingRouter = require('./controllers/testing')
 
 const mongoUrl = `mongodb+srv://end-user:Figueroa1907@firstcluster.6wq2ngu.mongodb.net/`
 mongoose.connect(mongoUrl)
@@ -13,11 +14,19 @@ mongoose.connect(mongoUrl)
 app.use(cors())
 app.use(express.json())
 
+
+
 app.use(middlewares.tokenExtractor)
 app.use(middlewares.userExtractor)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
+
 app.use(middlewares.errorHandler)
 
 module.exports = app
