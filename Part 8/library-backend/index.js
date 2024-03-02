d8dd9db641c1b80a362266c1bc467290de2a6a7f
Francisco Figueroa Casas <franciscofigueroacasas@gmail.com>
2024-03-02 19:13:24 -0300

    exercise 8.6 completed

--- Part 8/library-backend/index.js
+++ Part 8/library-backend/index.js
@@ -1,6 +1,7 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
 const { gql } = require('apollo-server')
+const { v1: uuid } = require('uuid')
 
 let authors = [
   {
@@ -113,6 +114,15 @@ const typeDefs = gql`
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book
+  }
 `
 
 const resolvers = {
@@ -136,6 +146,22 @@ const resolvers = {
     bookCount: (root) => {
       const bookCount = books.reduce((sum, current) => current.author === root.name ? sum = sum + 1 : sum, 0)
       return bookCount
+    }
+  },
+
+  Mutation: {
+    addBook: (root, args) => {
+      const foundAuthor = authors.find(author => author.name === args.author)
+      if (!foundAuthor){
+        const newAuthor = {
+          name: args.author, 
+          id: uuid()
+        }
+        authors = authors.concat(author)
+      }
+      const book = { ...args, id: uuid()}
+      books = books.concat(book)
+      return book
     }
   }
 }
