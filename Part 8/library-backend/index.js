140753223fc89fb5290cf8c5a298cf924f89c86d
Francisco Figueroa Casas <franciscofigueroacasas@gmail.com>
2024-03-02 18:51:02 -0300

    exercise 8.4 completed

--- Part 8/library-backend/index.js
+++ Part 8/library-backend/index.js
@@ -110,7 +110,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -119,14 +119,18 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author){
+        return books
+      }
+      return books.filter(book => book.author === args.author)
+    },
     allAuthors: () => authors
   },
 
   Author: {
     bookCount: (root) => {
       const bookCount = books.reduce((sum, current) => current.author === root.name ? sum = sum + 1 : sum, 0)
-      console.log(bookCount)
       return bookCount
     }
   }
